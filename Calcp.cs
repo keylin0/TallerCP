713c4c036896c94948eb4832cf41ed1666b85be3
keyla <keysugar2009@gmail.com>
2023-07-09 03:52:55 -0600

    Se abre un while para comprobar si la variable opciones es diferente a 6, mientras sea verdadero el bucle se ejecutara y continuara solicitando opciones +

--- Calcp.cs
+++ Calcp.cs
@@ -13,6 +13,32 @@
                              "5 - Limpiar \n" +
                              "6 - Salir \n";
             Console.WriteLine(menuc); //Luego imprimimos en consola el contenido de la variable
+            while (opciones != 6) //Abrimos un while para comprobar si la variable opciones es diferente de 6
+            {                   //(Mientras la condicion sea verdadera, el bucle se ejecutara y continuara solicitando opciones)
+                Console.WriteLine("Elija una opcion:" ); //Mostramos en consola para que elija una opcion del 1 al 6
+                opciones = Convert.ToInt32(Console.ReadLine()); //Con ambos metodos se lee la informacion del usuario para asignarlo a la variable opciones
+                if (opciones == 1) //Usamos la condicional if para determinar que accion realizar, en este caso la suma (opcion 1)
+                {
+                    Console.WriteLine("Digite un primer valor: ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digite un segundo valor: ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    int total = sumar (num1, num2); // LLamamos a la funcion sumar agregando los valores num1 y num2 para almacenarlo en la variable total
+                    Console.WriteLine("Total: " + total); // Mostramos mensaje + valor calculado por la suma
+                
+                }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            }
         }
     }
 }
