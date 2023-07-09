6619d42fd5c873240ee3f804da980bf2e4a5b07b
keyla <keysugar2009@gmail.com>
2023-07-09 03:19:08 -0600

    Se declara una variable entera (opciones) que inicializa su valor en 0, luego un string llamado menuc y la inicializo con un valor que representa la calculadora, para luego imprimir en consola el contenido de la variable.

--- Calcp.cs
+++ Calcp.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TCP
+﻿namespace Calculadora
 {
     internal class Calcp
     {
+        private static void Main(string[] args)
+        {
+            int opciones = 0; //Se declara una variable entera (opciones) que inicializa su valor en 0
+            string menuc = " **** Calculadora Menu **** \n" + // Luego un string llamado menuc y la inicializo con un valor que representa la calculadora
+                             "1 - Sumar \n" +
+                             "2 - Restar \n" +
+                             "3 - Multiplicar \n" +
+                             "4 - Dividir \n" +
+                             "5 - Limpiar \n" +
+                             "6 - Salir \n";
+            Console.WriteLine(menuc); //Luego imprimimos en consola el contenido de la variable
+        }
     }
 }
