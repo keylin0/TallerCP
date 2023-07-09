5fa8cc60777b701d0481dc9e449181e4bc202eb7
keyla <keysugar2009@gmail.com>
2023-07-09 04:23:49 -0600

    Termina la suma y se agrega la opcion 5 que seria limpiar pantalla

--- Calcp.cs
+++ Calcp.cs
@@ -15,7 +15,7 @@
             Console.WriteLine(menuc); //Luego imprimimos en consola el contenido de la variable
             while (opciones != 6) //Abrimos un while para comprobar si la variable opciones es diferente de 6
             {                   //(Mientras la condicion sea verdadera, el bucle se ejecutara y continuara solicitando opciones)
-                Console.WriteLine("Elija una opcion:" ); //Mostramos en consola para que elija una opcion del 1 al 6
+                Console.WriteLine("Elija una opcion:"); //Mostramos en consola para que elija una opcion del 1 al 6
                 opciones = Convert.ToInt32(Console.ReadLine()); //Con ambos metodos se lee la informacion del usuario para asignarlo a la variable opciones
                 if (opciones == 1) //Usamos la condicional if para determinar que accion realizar, en este caso la suma (opcion 1)
                 {
@@ -23,25 +23,18 @@
                     int num1 = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Digite un segundo valor: ");
                     int num2 = Convert.ToInt32(Console.ReadLine());
-                    int total = sumar (num1, num2); // LLamamos a la funcion sumar agregando los valores num1 y num2 para almacenarlo en la variable total
+                    int total = sumar(num1, num2); // LLamamos a la funcion sumar agregando los valores num1 y num2 para almacenarlo en la variable total
                     Console.WriteLine("Total: " + total); // Mostramos mensaje + valor calculado por la suma
                 }
+                else if (opciones == 5) //Si la variable opciones es igual a 5 se limpiara la pantalla
+                {
+                    Console.Clear(); //Limpiamos pantalla
+                }
+                Console.WriteLine(menuc); // Despues de limpiar pantalla mostramos en consola el menu nuevamente
                 int sumar(int num1, int num2) //Le damos valor a la funcion llamada sumar que toma 2 parametros de tipo entero
                 {
                     return num1 + num2; //La funcion suma los 2 valores y devuelve el resultado como un entero
                 }
-                
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             }
         }
     }
